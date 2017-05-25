da32eea2cfdcf49f15002e22fa739dffef3805e9
Ben Hall <bentomhall@gmail.com>
2017-05-24 20:11:33 -0400

    Initialized teacher-side app from shared project template.

---
+++ OPMTeacher/OPMTeacher/OPMTeacher/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using OPMTeacher.Models;
+
+namespace OPMTeacher.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item.Text;
+			Item = item;
+		}
+
+		int quantity = 1;
+		public int Quantity
+		{
+			get { return quantity; }
+			set { SetProperty(ref quantity, value); }
+		}
+	}
+}
