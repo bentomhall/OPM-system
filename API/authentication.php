3b8cbe4368b2843dc1a16739bef4ce3b2237252e
Ben Hall <bhall@tampaprep.org>
2017-05-25 09:05:01 -0400

    fixing missing semicolons

--- API/authentication.php
+++ API/authentication.php
@@ -11,8 +11,9 @@ function get_credential($username, $credential_type) {
     $db_credentials = get_database_config();
     $connection = new mysqli_connect($db_credentials[0], $db_credentials[1], $db_credentials[2]);
     if ($connection->connect_error) {
-        die("Connection failed: ".$connection->connect_errno)
+        die("Connection failed: ".$connection->connect_errno);
     }
+
     if ($credential_type == PASSWORD_AUTHENTICATION) {
         $query = $connection->prepare('SELECT hash FROM authentication WHERE username = ?');
         $query->bind_param('s', $username);
@@ -20,7 +21,7 @@ function get_credential($username, $credential_type) {
         if ($query->field_count == 1) {
             $result = $query->fetch_array(MYSQLI_ASSOC);
             $query->free();
-            return $result['hash']
+            return $result['hash'];
         }
     } else if ($credential_type == TOKEN_AUTHENTICATION) {
         $query = $connection->prepare('SELECT token FROM authentication WHERE username = ?');
@@ -29,7 +30,7 @@ function get_credential($username, $credential_type) {
         if ($query->field_count == 1) {
             $result = $query->fetch_array(MYSQLI_ASSOC);
             $query->free();
-            return $result['token']
+            return $result['token'];
         }
     }
     return false
@@ -46,7 +47,7 @@ function login($username, $password) {
 }
 
 function store_token($username) {
-    
+
 }
 
 function generate_token($username) {
