271b97eb2ba0616eb0b984a2a869940de281d66f
Ben Hall <bhall@tampaprep.org>
2017-05-25 09:54:44 -0400

    Adding model classes

---
+++ OPMStudent/OPMStudent/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace OPMStudent
+{
+    public class Question
+    {
+        public Question()
+        {
+        }
+
+        private string title;
+        private string text;
+        private string hint;
+        private string answer;
+        private Guid id;
+        private User Author;
+
+        public string Title { get => title; set => title = value; }
+        public string Text { get => text; set => text = value; }
+        public string Hint { get => hint; set => hint = value; }
+        public string Answer { get => answer; set => answer = value; }
+    }
+}
---
+++ OPMStudent/OPMStudent/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OPMStudent
+{
+    internal class User
+    {
+        internal User()
+        {
+            
+        }
+        public string Username { get;}
+        public int UserCode { get; }
+        private Guid id;
+    }
+}
